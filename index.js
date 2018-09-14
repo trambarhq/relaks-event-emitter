4a2244f0e43dd3bede433902bbf4f62f8c2c1d50
Chung Leong <chernyshevsky@hotmail.com>
2018-09-14 10:00:33 +0200

    Initial check-in.

---
+++ index.js
@@ -0,0 +1,130 @@
+function RelaksEventEmitter() {
+    this.listeners = [];
+}
+
+var prototype = RelaksEventEmitter.prototype;
+
+/**
+ * Attach an event handler
+ *
+ * @param  {String} type
+ * @param  {Function} handler
+ * @param  {Boolean|undefined} beginning
+ */
+prototype.addEventListener = function(type, handler, beginning) {
+    if (typeof(type) !== 'string') {
+        if (process.env.NODE_ENV !== 'production') {
+            console.warn('Invalid event type passed to addEventListener()');
+        }
+        return;
+    }
+    if (!(handler instanceof Function) && handler != null) {
+        if (process.env.NODE_ENV !== 'production') {
+            console.warn('Non-function passed to addEventListener()');
+        }
+        return;
+    }
+    if (beginning) {
+        this.listeners.unshift({ type: type,  handler: handler });
+    } else {
+        this.listeners.push({ type: type,  handler: handler });
+    }
+};
+
+/**
+ * Remove an event handler
+ *
+ * @param  {String} type
+ * @param  {Function} handler
+ */
+prototype.removeEventListener = function(type, handler) {
+    this.listeners = this.listeners.filter(function(listener) {
+        return !(listener.type === type && listener.handler === handler);
+    })
+};
+
+/**
+ * Send event to event listeners, return true or false depending on whether
+ * there were any listeners
+ *
+ * @param  {RelaksDjangoDataSourceEvent} evt
+ *
+ * @return {Boolean}
+ */
+prototype.triggerEvent = function(evt) {
+    var listeners = this.listeners.filter(function(listener) {
+        return (listener.type === evt.type);
+    });
+    if (listeners.length === 0) {
+        return false;
+    }
+    dispatchEvent(evt, listeners);
+    return true;
+};
+
+function dispatchEvent(evt, listeners) {
+    for (var i = 0; i < listeners.length; i++) {
+        var listener = listeners[i];
+        listener.handler.call(evt.target, evt);
+
+        if (this.defaultPostponed) {
+            var remainingListeners = listeners.slice(i + 1);
+            var promise = this.defaultPostponed.then(function(decision) {
+                if (decision === false) {
+                    evt.preventDefault();
+                }
+                if (!evt.propagationStopped) {
+                    return dispatchEvent(evt, remainingListeners);
+                }
+            });
+            if (!evt.decisionPromise) {
+                evt.decisionPromise = promise;
+            }
+            return promise;
+        }
+        if (evt.propagationStopped) {
+            break;
+        }
+    }
+}
+
+function GenericEvent(type, target, props) {
+    this.type = type;
+    this.target = target;
+    assign(this, props);
+    this.defaultPrevented = false;
+    this.defaultPostponed = null;
+    this.propagationStopped = false;
+    this.decisionPromise = null;
+}
+
+var prototype = GenericEvent.prototype;
+
+prototype.preventDefault = function() {
+    this.defaultPrevented = true;
+};
+
+prototype.postponeDefault = function(promise) {
+    if (!promise || !(promise.then instanceof Function)) {
+        if (process.env.NODE_ENV !== 'production') {
+            console.warn('Non-promise passed to postponeDefault()');
+        }
+        return;
+    }
+    this.defaultPostponed = promise;
+};
+
+prototype.stopImmediatePropagation = function() {
+    this.propagationStopped = true;
+};
+
+prototype.waitForDecision = function() {
+    if (!this.decisionPromise) {
+        return Promise.resolve();
+    }
+    return this.decisionPromise;
+};
+
+module.exports = RelaksEventEmitter;
+module.exports.RelaksEventEmitter = RelaksEventEmitter;
+module.exports.GenericEvent = GenericEvent;
---
+++ test/postpone-default.test.js
@@ -0,0 +1,14 @@
+import Chai, { expect } from 'chai';
+import ChaiAsPromised from 'chai-as-promised';
+import RelaksEventEmitter, { GenericEvent } from '../index';
+
+Chai.use(ChaiAsPromised);
+
+describe('#postponeDefault()', function() {
+    it ('should postpone default behavior', function() {
+    })
+    it ('should prevent default behavior when promise is fulfilled with false', function() {
+    })
+    it ('should keep other handlers from being called', function() {
+    })
+})
